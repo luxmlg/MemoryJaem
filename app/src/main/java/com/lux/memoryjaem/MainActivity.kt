c47c234804f27368e012a229983e072f8f9a6253
Luka Maisuradze <lmaisuradz3@gmail.com>
2022-06-27 14:24:47 +0400

    Initial commit

---
+++ app/src/main/java/com/lux/memoryjaem/MainActivity.kt
@@ -0,0 +1,151 @@
+package com.lux.memoryjaem
+
+import android.animation.ArgbEvaluator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.lux.memoryjaem.models.BoardSize
+import com.lux.memoryjaem.models.MemoryGame
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var clRoot: CoordinatorLayout
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
+    private var gameName: String? = null
+    private lateinit var memoryGame: MemoryGame
+    private lateinit var adapter: MemoryBoardAdapter
+    private var boardSize = BoardSize.EASY
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        clRoot = findViewById(R.id.clRoot)
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
+
+        setupBoard()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.mi_refresh -> {
+                if (memoryGame.getNumMoves() > 0 && !memoryGame.haveWonGame()) {
+                    showAlertDialog(resources.getString(R.string.quit_game), null) {
+                        setupBoard()
+                    }
+                } else {
+                    setupBoard()
+                }
+                return true
+            }
+            R.id.mi_new_size -> {
+                showNewSizeDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun showNewSizeDialog() {
+        val boardSizeView = LayoutInflater.from(this).inflate(R.layout.dialog_board_size, null)
+        val radioGroupSize = boardSizeView.findViewById<RadioGroup>(R.id.radioGroupSize)
+        when (boardSize) {
+            BoardSize.EASY -> radioGroupSize.check(R.id.rbEasy)
+            BoardSize.MEDIUM -> radioGroupSize.check(R.id.rbMedium)
+            BoardSize.HARD -> radioGroupSize.check(R.id.rbHard)
+        }
+        showAlertDialog(resources.getString(R.string.choose_size), boardSizeView) {
+            boardSize = when (radioGroupSize.checkedRadioButtonId) {
+                R.id.rbEasy -> BoardSize.EASY
+                R.id.rbMedium -> BoardSize.MEDIUM
+                else -> BoardSize.HARD
+            }
+            gameName = null
+            setupBoard()
+        }
+    }
+
+    private fun showAlertDialog(title: String, view: View?, positiveClickListener: View.OnClickListener) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton(resources.getString(R.string.cancel), null)
+            .setPositiveButton(resources.getString(R.string.ok)) { _, _ ->
+                positiveClickListener.onClick(null)
+            }.show()
+    }
+
+    private fun setupBoard() {
+        supportActionBar?.title = gameName ?: getString(R.string.app_name)
+        memoryGame = MemoryGame(boardSize)
+        when (boardSize) {
+            BoardSize.EASY -> {
+                tvNumMoves.text = resources.getString(R.string.easy_moves)
+                tvNumPairs.text = resources.getString(R.string.easy_pairs)
+            }
+            BoardSize.MEDIUM -> {
+                tvNumMoves.text = resources.getString(R.string.medium_moves)
+                tvNumPairs.text = resources.getString(R.string.medium_pairs)
+            }
+            BoardSize.HARD -> {
+                tvNumMoves.text = resources.getString(R.string.hard_moves)
+                tvNumPairs.text = resources.getString(R.string.hard_pairs)
+            }
+        }
+        tvNumPairs.setTextColor(ContextCompat.getColor(this, R.color.color_progress_none))
+        adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener {
+            override fun onCardClicked(position: Int) {
+                updateGameWithFlip(position)
+            }
+        })
+        rvBoard.adapter = adapter
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    private fun updateGameWithFlip(position: Int) {
+        if (memoryGame.haveWonGame()) {
+            Snackbar.make(clRoot, resources.getString(R.string.already_won), Snackbar.LENGTH_LONG).show()
+            return
+        }
+        if (memoryGame.isCardFaceUp(position)) {
+            Snackbar.make(clRoot, resources.getString(R.string.invalid_move), Snackbar.LENGTH_SHORT).show()
+            return
+        }
+
+        if (memoryGame.flipCard(position)) {
+            val color = ArgbEvaluator().evaluate(
+                memoryGame.numPairsFound.toFloat() / boardSize.getNumPairs(),
+                ContextCompat.getColor(this, R.color.color_progress_none),
+                ContextCompat.getColor(this, R.color.color_progress_full)
+            ) as Int
+            tvNumPairs.setTextColor(color)
+            tvNumPairs.text = String.format(resources.getString(R.string.pairs), memoryGame.numPairsFound, boardSize.getNumPairs())
+            if (memoryGame.haveWonGame()) {
+                Snackbar.make(clRoot, resources.getString(R.string.win), Snackbar.LENGTH_LONG).show()
+            }
+        }
+        tvNumMoves.text = String.format(resources.getString(R.string.moves), memoryGame.getNumMoves())
+        adapter.notifyDataSetChanged()
+    }
+}
